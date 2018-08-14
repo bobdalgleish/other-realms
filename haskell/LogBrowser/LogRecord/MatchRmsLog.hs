eb5d7199da03e747dc32912569539491b419d230
Bob Dalgleish <dalgleish@sedsystems.ca>
2018-08-14 08:32:00 -0600

    Match single log record. RDD
    Can now match single log record.

--- haskell/LogBrowser/LogRecord/MatchRmsLog.hs
+++ haskell/LogBrowser/LogRecord/MatchRmsLog.hs
@@ -8,7 +8,7 @@ import Data.List (break)
 matchRecord :: (Maybe (Log String)) -> String -> [Log String]
 matchRecord prior line =
   case convertToDateTime line of
-    Just ts -> let partial = matchApplication (Log { timestamp = Timestamp ts })  (drop 22 line)
+    Just ts -> let partial = matchApplication (Log { timestamp = Timestamp ts, source = Source "RMS1" })  (drop 22 line)
                in case prior of
                     Just priorLog -> [priorLog,partial]
                     Nothing -> [partial]
@@ -21,15 +21,19 @@ mergeBody log line = let (Body bdy) = body log
                          newBody = bdy ++ "\n" ++ line
                      in log { body = Body newBody }
 
+-- |match the application, thread fields and start parsing the body
 matchApplication :: Log String -> String -> Log String
 matchApplication log line =
   let (app, appr) = break (==':') line
       (thr, thrr) = break (=='|') (tail appr)
   in matchBody (log {application = Application app, thread = Thread thr}) (tail thrr)
 
+-- |match the body portion, followed by the postamble
 matchBody :: Log String -> String -> Log String
 matchBody log line =
-  let (bdy, meths) = breakEnd '|' line
+  let allBody = line
+      parts = splitBy ('|'==) allBody
+      (bdy, meths) = breakEnd ('|'==) allBody
       (mth:mod:lno:lvl:[]) = splitBy (':'==) meths
   in log { body = Body bdy,
            methodName = MethodName mth,
@@ -37,13 +41,21 @@ matchBody log line =
            moduleLineNo = MethodLineNumber lno,
            logLevel = toLogLevel lvl
          }
-  where
-    breakEnd c s = let p = splitBy (c==) s
-                       l = length p
-                       b = last p
-                       f = if l > 1 then (take (1 + length b) s) else []
-                   in (f, b)
 
+getPostAmble :: String -> Maybe (String, String, String, String)
+getPostAmble post =
+  let parts = splitBy (':'==) post
+  if length parts != 4 then Nothing else let (mth: mod: lno: lvl: []) = parts in Just (mth, mod, lno, lvl) 
+
+-- |Split on the last matching character
+breakEnd :: (Char -> Bool) -> String -> (String, String)
+breakEnd c s = let parts = splitBy c s
+                   len   = length parts
+                   back  = last parts
+                   front = if len > 1 then (take (length s - 1 - length back) s) else []
+               in (front, back)
+
+-- |Split on the matching character
 splitBy :: (Char -> Bool) -> String -> [String]
 splitBy _ [] = []
 splitBy p s = let (front, back) = break p s
