21cb0027cbe448608648068943c450bfadf97ac3
Bob Dalgleish <dalgleish@sedsystems.ca>
2018-08-30 10:42:47 -0600

    Use string interpolation for some generation. RDD

--- haskell/codegen/src/Record/Data.hs
+++ haskell/codegen/src/Record/Data.hs
@@ -1,14 +1,15 @@
+{-# LANGUAGE QuasiQuotes #-}
 module Record.Data where
 
 import Data.List
+import Data.Char
+import Data.String.Interpolate
 
-data Field = Field String String String
+data Field = Field { fieldName :: String        -- ^name of the field
+                   , fieldType :: String        -- ^type of the field
+                   , fieldDescription :: String -- ^description of the field
+                   }
                    deriving Show
-
-fieldName, fieldType, fieldDescription :: Field -> String
-fieldName (Field n _ _) = n
-fieldType (Field _ t _) = t
-fieldDescription (Field _ _ d) = d
 
 -- |Describe a data structure
 data Record = Record { recordName :: String
@@ -27,42 +28,123 @@ generateCode :: RecordFamily -> [Record] -> String
 generateCode family records = show $ map (\r -> unlines $ genRecord family r) records
                               
 genRecord :: RecordFamily -> Record -> [String]
-genRecord family record = [ "File " ++ recordName record ++ ".h:" ]
+genRecord family record = let base = baseFileName $ recordName record
+                in
+                    [ [i|File #{base}.h:|] ]
                     ++ fileHeader
+                    ++ dupPreventer ((tail $ namespace record) ++ (fileNameFragments $ recordName record) ++ ["h"])
                     ++ (dumpIncludes $ sharedIncludes family)
+                    ++ [""]
                     ++ (dumpNameSpace $ namespace record)
-                    ++ ["class " ++ recordName record ++ "{"]
+                    ++ ["", "class " ++ recordName record ++ "{"]
                     ++ ["private:"]
                     ++ (map declareField $ fields record)
-{-}                    , "public:"
-                    , dumpConstructor (recordName record) (fields record)
--}
+                    ++ ["", "public:"]
+                    ++ (dumpConstructor (recordName record) (fields record))
+                    ++ (boilerPlate1 (recordName record))
+                    ++ (dumpGetters (fields record))
                     ++ ["};"]
                     ++ (endNameSpace $ namespace record)
+                    ++ ["#endif"]
 
+baseFileName :: String -> String
+baseFileName cls = map toLower $ intercalate "_" (fileNameFragments cls)
+
+fileNameFragments :: String -> [String]
+fileNameFragments [] = []
+fileNameFragments s = let (pre, post) = break isUpper s
+                 in case pre of
+                    [] -> let r = tail post
+                              (rf, rb) = break isUpper r
+                          in (head post:rf) : fileNameFragments rb
+                    word -> word : fileNameFragments post
+
+include inc = [i|#include <#{inc}>|]
 
 dumpIncludes :: [String] -> [String]
-dumpIncludes i = map (\include -> "#include <" ++ include ++ ">") i
+dumpIncludes i = map (\inc -> include inc) i
 
-fileHeader = ["/** first line", " * Copyright 2018", " */"]
+fileHeader = [ "/************************************************************************"
+             , " *                                                                       *"
+             , " *   Contractor: SED SYSTEMS, SASKATOON, SASKATCHEWAN, CANADA            *"
+             , " *               (C) 2018 SED Systems, A Division of Calian Ltd.         *"
+             , " *               All rights reserved.                                    *"
+             , " *                                                                       *"
+             , " ************************************************************************/"
+             , ""
+             ]
+
+dupPreventer :: [String] -> [String]
+dupPreventer s = let preventer = (intercalate "_" $ map (\n -> map toUpper n) s) ++ "_"
+                 in [[i|#ifndef #{preventer}|], [i|#define #{preventer}|], ""]
 
 dumpNameSpace :: [String] -> [String]
-dumpNameSpace n = map (\namespace -> "namespace " ++ namespace ++ " {") n
+dumpNameSpace n = map (\namespace -> [i|namespace #{namespace} {|]) n
 
 endNameSpace :: [String] -> [String]
-endNameSpace n = map (\namespace -> "} /* " ++ namespace ++ " */") n
+endNameSpace n = "" : (map (\namespace -> [i|} /* #{namespace} */|]) $ reverse n)
 
 declareField :: Field -> String
-declareField fld = "    " ++ fieldType fld ++ " " ++ fieldName fld ++ "; //<" ++ fieldDescription fld
+declareField fld = [i|    #{fieldType fld} _#{fieldName fld}; //<#{fieldDescription fld}|]
+
+indent :: String -> String
+indent s = "    " ++ s
+
+indentN :: Int -> String
+indentN n = intercalate "" $ replicate n "    "
+
+dumpConstructor :: String -> [Field] -> [String]
+dumpConstructor cls flds =  [
+                              indent $ cls ++ "();"
+                            , ""
+                            , indent $ cls ++ "("
+                            ] ++ 
+                            dumpFormalParameters flds ++
+                            [
+                              indent $ ");"
+                            ]
+
+boilerPlate1 :: String -> [String]
+boilerPlate1 cls = [[i|
+    virtual ~#{cls}();
+
+    #{cls}(const uint8_t* buff, std::size_t availLength, std::size_t& used );
+
+    bool merge( const #{cls}& that );
+    
+    bool operate==( const #{cls}& that );
+|]]
+
+dumpFormalParameters :: [Field] -> [String]
+dumpFormalParameters flds = let fieldUse = map (\f -> [i|        #{fieldType f} #{fieldName f}|]) flds
+                                l = length fieldUse
+                            in (map (++ ",") (init fieldUse)) ++ (drop (l-1) fieldUse)
+
+firstToUpper :: String -> String
+firstToUpper (f:r) = (if isLower f then toUpper f else f) : r
+
+dumpGetters :: [Field] -> [String]
+dumpGetters flds = map (\f -> indent $ "const " ++ fieldType f ++ "& get" ++ (firstToUpper $ fieldName f) ++ "() const;" ) flds
+
+dumpDestructor :: String -> [String]
+dumpDestructor cls = ["", indent $ "virtual ~" ++ cls ++ "();", ""]
 
 recordFamily = RecordFamily { recordFamilyName = "family",
                               sharedIncludes = ["string", "fstream"]
                             }
                                  
-record1 = Record { recordName = "LogCtrl"
+record1 = Record { recordName = "RpdLogCtrl"
                  , fields = [
-                     Field "first" "char" "first character"
-                 ]
-                 , namespace = ["sedssystems", "gcp"]
-                 , includes = ["gcp/ccap/def/rcp_field_types.h"]
+                              Field "first" "char" "first character"
+                            , Field "second" "int" "an int"
+                            ]
+                 , namespace = ["sedsystems", "gcp", "message", "rcp", "def"]
+                 , includes = [
+                                "message/rcp/rcp.h"
+                              , "gcp/message/rcp/def/rcp_field_types.h"
+                              , "docsis/common/tlv/def/tlv_complex.h"
+                              ]
                  }
+
+main = do
+    putStr $ unlines $ genRecord recordFamily record1
